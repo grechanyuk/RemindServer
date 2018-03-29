d14605e4d7d909c6660c4c4577d66dac69c50959
grechanyuk <xorolllo@hotmail.com>
2018-03-29 11:20:12 +0500

    Converter JSON Added

--- src/main/java/com/developer/eg/remind/server/controller/RemindController.java
+++ src/main/java/com/developer/eg/remind/server/controller/RemindController.java
@@ -1,18 +1,30 @@
 package com.developer.eg.remind.server.controller;
 
+import com.developer.eg.remind.server.entity.Remind;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Date;
+
+@RestController //говорит Spring'у, чтобы тот отдавал JSON объекты
 @RequestMapping("/reminder")
 public class RemindController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getReminder() {
-        return "My reminder";
+    public Remind getReminder() {
+        return createMockRemind();
+    }
+
+    private Remind createMockRemind() {
+        Remind remind = new Remind();
+        remind.setId(1);
+        remind.setRemindDate(new Date());
+        remind.setTitle("First object");
+        return remind;
     }
 
 }
